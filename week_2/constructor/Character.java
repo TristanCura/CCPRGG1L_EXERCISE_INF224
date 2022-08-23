48ccad696ee217b7a5de926b0286f0d1bedd0c74
Tristan Cura <111546355+TristanCura@users.noreply.github.com>
2022-08-22 20:32:15 -0700

    Create Character.java

---
+++ week_2/constructor/Character.java
@@ -0,0 +1,51 @@
+public class Character{
+    int strength;
+    int agility;
+    int intelligence;
+    int HP;
+    String SPCITM;
+    String name;
+    
+    public void sayMyName(){
+        System.out.println("\n*Hello I am " + "'" + name + "'...");
+    }
+
+    public void sayMyStrength() {
+        System.out.println("\t>My Strength is " + strength);
+    }
+
+    public void sayMyAgiliy() {
+        System.out.println("\t>My Agility is " + agility);
+    }
+    public void sayMyIntelligence() {
+        System.out.println("\t>My Intelligence is " + intelligence);
+    }
+
+    public void sayMyHP() {
+        System.out.println("\t>My Base HP is " + HP);
+    }
+
+    public void sayMySpecialITM() {
+        System.out.println("\t>My Special Item is " + SPCITM);
+    }
+    
+//--------------------------------------------------------------
+
+    public void sayMyAttributes(){
+        sayMyName();
+        sayMyStrength();
+        sayMyAgiliy();
+        sayMyIntelligence();
+        sayMyHP();
+    }
+ 
+//--------------------------------------------------------------
+
+    public Character(int str, int agi, int intel, int hp, String charname){
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        HP = hp;
+        name = charname;
+    }
+}
