2bb5cd6231251df25b8cd41c77936a4342a3d1fc
Tristan Cura <111546355+TristanCura@users.noreply.github.com>
2022-09-12 19:49:17 -0700

    Create App.java

---
+++ week_5/App.java
@@ -0,0 +1,94 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        App myApp = new App();
+
+        // Non-static method call
+        myApp.showHelloJava();
+
+        // Static method call
+        showHelloWorld();
+
+        // Non-static method call with parameter
+        myApp.showName("Tristan");
+
+        // Static method call with parameter
+        showAge(20);
+
+        // Static method call with two parameters
+        showNameAndAge("Tristan", 18);
+
+        // Non-static method call with two parameters that returns an int
+        int sum = myApp.add(1, 1);
+        System.out.println(sum);
+
+        // Static method call with two parameters that returns an int
+        int difference = subtract(1, 1);
+        System.out.println(difference);
+        
+        int aa = multiply(1, 5);
+        System.out.println(aa);
+        
+        int bb = divide(5, 1);
+        System.out.println(bb);
+        
+        boolean cc = isEqual(12, 12);
+        System.out.println(cc);
+
+     /*===================================================================*/
+        
+    }
+
+    // Non-static method
+    void showHelloJava() {
+        System.out.println("Hello Java! This is a non-static method.");
+    }
+
+    // Static method
+    static void showHelloWorld() {
+        System.out.println("Hello World! This is a static method.");
+    }
+
+    // Non-static method with a parameter
+    void showName(String name) {
+        System.out.println("My name is " + name);
+    }
+
+    // Static method with a parameter
+    static void showAge(int age) {
+        System.out.println("My age is " + age);
+    }
+
+    // Static method with two parameters
+    static void showNameAndAge(String name, int age) {
+        System.out.println("Hello: " + name);
+        System.out.println("You are : " + age + " years old!");
+    }
+
+    // Non-static method with two parameters with a return type
+    int add(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+
+    // Static method with two parameters with a return type
+    static int subtract(int x, int y) {
+        int z = x - y;
+        return z;
+    }
+    
+    static int multiply(int x, int y) {
+        int z = x * y;
+        return z;
+    }
+    
+    static int divide(int x, int y) {
+        int z = x / y;
+        return z;
+    }
+    
+    static boolean isEqual(int x, int y) {
+    	boolean z = x==y;
+    	return z;
+    }
+}
